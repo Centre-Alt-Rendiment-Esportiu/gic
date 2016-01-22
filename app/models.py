74c2aff2c2effad1ab0109131070906d46e55fe6
danipknot <daniruizmartinez@icloud.com>
2016-01-22 14:10:09 +0100

    probes gecar

--- app/models.py
+++ app/models.py
@@ -146,125 +146,131 @@ class GIC_ROL(db.Model):
         self.fi = fi
 
 class GE_CAR_PERSONA(db.Model):
-    """taula ge car"""
     __tablename__ = 'A_GE_CAR_PERSONA'
     identificador = db.Column(db.String(20), primary_key=True)
     foto = db.Column(db.String(30))
-    dni = db.Column(db.String(9))
-    passaport = db.Column(db.String(20))
-    nom = db.Column(db.String(20))
-    cognom1 = db.Column(db.String(20))
-    cognom2 = db.Column(db.String(20))
-    sexe = db.Column(db.Integer)
-    ss = db.Column(db.String(20))
-    tipus = db.Column(db.Integer)
-    data_neix = db.Column(db.Date)
-    lloc_neix = db.Column(db.String(30))
-    provincia_neix = db.Column(db.Integer)
-    comarca_neix = db.Column(db.Integer)
-    auto_neix = db.Column(db.Integer)
-    pais_neix = db.Column(db.Integer)
-    direccio = db.Column(db.String(40))
-    poblacio = db.Column(db.String(30))
-    provincia = db.Column(db.Integer)
-    cp = db.Column(db.String(15))
-    comarca = db.Column(db.Integer)
-    autonomia = db.Column(db.Integer)
-    pais = db.Column(db.Integer)
-    telefon1 = db.Column(db.String(20))
-    telefon2 = db.Column(db.String(20))
-    e_mail = db.Column(db.String(52))
-    estudis_act = db.Column(db.String(30))
-    nivel_academic = db.Column(db.Integer)
-    tipus_centre = db.Column(db.Integer)
-    nom_centre = db.Column(db.String(50))
-    aceptacio = db.Column(db.Integer)
-    revisiom = db.Column(db.Integer)
-    revisiops = db.Column(db.Integer)
-    fitxacomplerta = db.Column(db.Integer)
-    vehicle = db.Column(db.String(30))
-    atricula = db.Column(db.String(10))
-    tutor1 = db.Column(db.String(30))
-    contacto1 = db.Column(db.String(30))
-    tutor2 = db.Column(db.String(30))
-    contacto2 = db.Column(db.String(30))
-    actiu = db.Column(db.Integer)
-    identificador_ant = db.Column(db.String(15))
-    id_med = db.Column(db.Integer)
-    id_fis = db.Column(db.Integer)
-    id_psi = db.Column(db.Integer)
-    cip = db.Column(db.String(25))
-    consentiment = db.Column(db.Integer)
-    data_consentiment = db.Column(db.Date)
-    data_revisiom = db.Column(db.Date)
-    consentiment_dad = db.Column(db.Integer)
-    consentiment_proinf = db.Column(db.Integer)
-    pro_sal_es = db.Column(db.Integer)
-    e_mail2 = db.Column(db.String(52))
-    password = db.Column(db.String(128))
-    salt = db.Column(db.String(20))		
-    def __init__(self, foto, dni, passaport, nom, cognom1, cognom2, sexe, ss \
-    , tipus, data_neix, lloc_neix, provincia_neix, comarca_neix, auto_neix, pais_neix \
-    , direccio, poblacio, provincia, cp, comarca, autonomia, pais, telefon1, telefon2 \
-    , e_mail, estudis_act, nivel_academic, tipus_centre, nom_centre, aceptacio \
-    , revisiom, revisiops, fitxacomplerta, vehicle, matricula, tutor1, contacto1 \
-    , tutor2, contacto2, actiu, identificador_ant, id_med, id_fis, id_psi, cip \
-    , consentiment, data_consentiment, data_revisiom, consentiment_dad \
-    , consentiment_proinf, pro_sal_es, e_mail2, password, salt):
+    def __init__(self, foto):
         self.foto = foto
-        self.passaport = passaport
-        self.nom = nom
-        self.cognom1 = cognom1
-        self.cognom2 = cognom2
-        self.sexe = sexe
-        self.ss = ss
-        self.tipus = tipus
-        self.data_neix = data_neix
-        self.lloc_neix = lloc_neix
-        self.provincia_neix = provincia_neix
-        self.comarca_neix = comarca_neix
-        self.auto_neix = auto_neix
-        self.pais_neix = pais_neix
-        self.direccio = direccio
-        self.poblacio = poblacio
-        self.provincia = provincia
-        self.cp = cp
-        self.comarca = comarca
-        self.autonomia = autonomia
-        self.pais = pais
-        self.telefon1 = telefon1
-        self.telefon2 = telefon2
-        self.e_mail = e_mail
-        self.estudis_act = estudis_act
-        self.nivel_academic = nivel_academic
-        self.tipus_centre = tipus_centre
-        self.nom_centre = nom_centre
-        self.aceptacio = aceptacio
-        self.revisiom = revisiom
-        self.revisiops = revisiops
-        self.fitxacomplerta = fitxacomplerta
-        self.vehicle = vehicle
-        self.matricula = matricula
-        self.tutor1 = tutor1
-        self.contacto1 = contacto1
-        self.tutor2 = tutor2
-        self.contacto2 = contacto2
-        self.actiu = actiu
-        self.identificador_ant = identificador_ant
-        self.id_med = id_med
-        self.id_fis = id_fis
-        self.id_psi = id_psi
-        self.cip = cip
-        self.consentiment_proinf = consentiment_proinf
-        self.pro_sal_es = pro_sal_es
-        self.e_mail2 = e_mail2
-        self.password = set_password(password)
-        self.salt = salt
-        def set_password(self, password):
-            self.password = hashlib.sha256('[B@3f13a310' + password).hexdigest()
-        def check_password(self, password):
-            if self.password == hashlib.sha256('[B@3f13a310' + password).hexdigest():
-                return True
-            else:
-                return False
+#class GE_CAR_PERSONA(db.Model):
+#    """taula ge car"""
+#    __tablename__ = 'A_GE_CAR_PERSONA'
+#    identificador = db.Column(db.String(20), primary_key=True)
+#    foto = db.Column(db.String(30))
+#    dni = db.Column(db.String(9))
+#    passaport = db.Column(db.String(20))
+#    nom = db.Column(db.String(20))
+#    cognom1 = db.Column(db.String(20))
+#    cognom2 = db.Column(db.String(20))
+#    sexe = db.Column(db.Integer)
+#    ss = db.Column(db.String(20))
+#    tipus = db.Column(db.Integer)
+#    data_neix = db.Column(db.Date)
+#    lloc_neix = db.Column(db.String(30))
+#    provincia_neix = db.Column(db.Integer)
+#    comarca_neix = db.Column(db.Integer)
+#    auto_neix = db.Column(db.Integer)
+#    pais_neix = db.Column(db.Integer)
+#    direccio = db.Column(db.String(40))
+#    poblacio = db.Column(db.String(30))
+#    provincia = db.Column(db.Integer)
+#    cp = db.Column(db.String(15))
+#    comarca = db.Column(db.Integer)
+#    autonomia = db.Column(db.Integer)
+#    pais = db.Column(db.Integer)
+#    telefon1 = db.Column(db.String(20))
+#    telefon2 = db.Column(db.String(20))
+#    e_mail = db.Column(db.String(52))
+#    estudis_act = db.Column(db.String(30))
+#    nivel_academic = db.Column(db.Integer)
+#    tipus_centre = db.Column(db.Integer)
+#    nom_centre = db.Column(db.String(50))
+#    aceptacio = db.Column(db.Integer)
+#    revisiom = db.Column(db.Integer)
+#    revisiops = db.Column(db.Integer)
+#    fitxacomplerta = db.Column(db.Integer)
+#    vehicle = db.Column(db.String(30))
+#    atricula = db.Column(db.String(10))
+#    tutor1 = db.Column(db.String(30))
+#    contacto1 = db.Column(db.String(30))
+#    tutor2 = db.Column(db.String(30))
+#    contacto2 = db.Column(db.String(30))
+#    actiu = db.Column(db.Integer)
+#    identificador_ant = db.Column(db.String(15))
+#    id_med = db.Column(db.Integer)
+#    id_fis = db.Column(db.Integer)
+#    id_psi = db.Column(db.Integer)
+#    cip = db.Column(db.String(25))
+#    consentiment = db.Column(db.Integer)
+#    data_consentiment = db.Column(db.Date)
+#    data_revisiom = db.Column(db.Date)
+#    consentiment_dad = db.Column(db.Integer)
+#    consentiment_proinf = db.Column(db.Integer)
+#    pro_sal_es = db.Column(db.Integer)
+#    e_mail2 = db.Column(db.String(52))
+#    password = db.Column(db.String(128))
+#    salt = db.Column(db.String(20))		
+#    def __init__(self, foto, dni, passaport, nom, cognom1, cognom2, sexe, ss \
+#    , tipus, data_neix, lloc_neix, provincia_neix, comarca_neix, auto_neix, pais_neix \
+#    , direccio, poblacio, provincia, cp, comarca, autonomia, pais, telefon1, telefon2 \
+#    , e_mail, estudis_act, nivel_academic, tipus_centre, nom_centre, aceptacio \
+#    , revisiom, revisiops, fitxacomplerta, vehicle, matricula, tutor1, contacto1 \
+#    , tutor2, contacto2, actiu, identificador_ant, id_med, id_fis, id_psi, cip \
+#    , consentiment, data_consentiment, data_revisiom, consentiment_dad \
+#    , consentiment_proinf, pro_sal_es, e_mail2, password, salt):
+#        self.foto = foto
+#        self.passaport = passaport
+#        self.nom = nom
+#        self.cognom1 = cognom1
+#        self.cognom2 = cognom2
+#        self.sexe = sexe
+#        self.ss = ss
+#        self.tipus = tipus
+#        self.data_neix = data_neix
+#        self.lloc_neix = lloc_neix
+#        self.provincia_neix = provincia_neix
+#        self.comarca_neix = comarca_neix
+#        self.auto_neix = auto_neix
+#        self.pais_neix = pais_neix
+#        self.direccio = direccio
+#        self.poblacio = poblacio
+#        self.provincia = provincia
+#        self.cp = cp
+#        self.comarca = comarca
+#        self.autonomia = autonomia
+#        self.pais = pais
+#        self.telefon1 = telefon1
+#        self.telefon2 = telefon2
+#        self.e_mail = e_mail
+#        self.estudis_act = estudis_act
+#        self.nivel_academic = nivel_academic
+#        self.tipus_centre = tipus_centre
+#        self.nom_centre = nom_centre
+#        self.aceptacio = aceptacio
+#        self.revisiom = revisiom
+#        self.revisiops = revisiops
+#        self.fitxacomplerta = fitxacomplerta
+#        self.vehicle = vehicle
+#        self.matricula = matricula
+#        self.tutor1 = tutor1
+#        self.contacto1 = contacto1
+#        self.tutor2 = tutor2
+#        self.contacto2 = contacto2
+#        self.actiu = actiu
+#        self.identificador_ant = identificador_ant
+#        self.id_med = id_med
+#        self.id_fis = id_fis
+#        self.id_psi = id_psi
+#        self.cip = cip
+#        self.consentiment_proinf = consentiment_proinf
+#        self.pro_sal_es = pro_sal_es
+#        self.e_mail2 = e_mail2
+#        self.password = set_password(password)
+#        self.salt = salt
+#        def set_password(self, password):
+#            self.password = hashlib.sha256('[B@3f13a310' + password).hexdigest()
+#        def check_password(self, password):
+#            if self.password == hashlib.sha256('[B@3f13a310' + password).hexdigest():
+#                return True
+#            else:
+#                return False
 
